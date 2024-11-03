a8b8e0985ff56f6d9b2b4caf5657ac30a1b392d0
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-11-03 14:53:18 +0530

    about description and mission updated

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -18,13 +18,36 @@ export default function AboutUs() {
 
   return (
     <div>
-      <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-primary mt-16 mb-16 pl-8 sm:pl-16 md:pl-28">
-        Our Team
-      </h1>
-      <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-16 mb-24">
-        {members.map((member, index) => (
-          <MemberProfile key={index} name={member.name} imageSrc={member.imageSrc} bio={member.bio}/>
-        ))}
+      <div className="bg-white p-10 md:p-16 md:pb-24 md:pt-24 space-y-10">
+        <div>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-10">About Durgasakha</h1>
+          <p className="mt-2 text-sm md:text-lg border-l-4 border-secondary pl-4 md:pl-8 text-justify max-w-7xl">
+            The feet started roaring and began to wander from the footsteps of the helpless. Gifts of Forts, Sea Forts, Bhuikot, Mountains and Peaks started going on. 
+            The neglect and the irony that tourists are making this invaluable historic record that they were wandering in the eyes and mind while enjoying it. 
+            Often, despite being face down to the eyes, it does not tolerate. Such a condition was happening.
+            “DURGASAKHA” was born from the same pain.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-10">Our Mission</h1>
+          <p className="mt-2 text-sm md:text-lg border-l-4 border-secondary pl-4 md:pl-8 text-justify max-w-7xl">
+            The feet started roaring and began to wander from the footsteps of the helpless.
+            Gifts of Forts, Sea Forts, Bhuikot, Mountains and Peaks started going on. 
+            The neglect and the irony that tourists are making this invaluable historic record that they were wandering in the eyes and mind while enjoying it. 
+            Often, despite being face down to the eyes, it does not tolerate. Such a condition was happening.
+            “DURGASAKHA” was born from the same pain.
+          </p>
+        </div>
+      </div>
+      <div className="p-10 md:pb-20 md:pt-20">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary sm:pl-8 pb-8 sm:pb-12">
+          Our Team
+        </h1>
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {members.map((member, index) => (
+            <MemberProfile key={index} name={member.name} imageSrc={member.imageSrc} bio={member.bio}/>
+          ))}
+        </div>
       </div>
     </div>
   );
