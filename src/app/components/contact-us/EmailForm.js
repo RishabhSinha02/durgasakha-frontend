56b0d925e2a755e4e77eb118bf1c93c17db4010d
Rishabh Sinha <sinharishabh2002@gmail.com>
2024-11-17 18:52:34 +0530

    Contact Us Page Fix

--- src/app/components/contact-us/EmailForm.js
+++ src/app/components/contact-us/EmailForm.js
@@ -4,16 +4,18 @@ import { Button, TextInput, Textarea } from "flowbite-react";
 
 export function EmailForm() {
   return (
-    <form className="flex min-w-96 md:w-3/4 flex-col gap-6 bg-white p-8 rounded-lg">
+    <form className="flex min-w-96 md:w-3/4 flex-col gap-6 bg-white p-8 rounded-lg relative -top-24">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             <TextInput id="email" type="name" placeholder="Full Name" required />
             <TextInput id="name" type="email" placeholder="Email Address" required />
             <TextInput id="mobile" type="number" placeholder="Phone Number" required />
         </div>
         <div>
-            <Textarea id="message" placeholder="Enter Message" required />
+            <Textarea id="message" className="h-36" placeholder="Enter Message" required />
         </div>
-        <Button type="submit">Submit</Button>
+        <div className="flex justify-center">
+            <Button type="submit" className="rounded-full" variant="primary">Send Message</Button>
+        </div>
     </form>
   );
 }
--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -6,34 +6,33 @@ import { BsTelephone } from "react-icons/bs";
 import { IoLocationOutline } from "react-icons/io5";
 
 export default function ContactUs() {
-    return (
-      <div className="flex flex-col items-center gap-12 border-red-600 pb-10">
-        <Map />
-        <EmailForm />
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          <ContactCard 
-            icon={<MdOutlineEmail size={50}/>} 
-            title="Email Address" 
-            subtitle="info@durgasakha.com" 
-            callToActionText="Send Email"
-            callToActionLink="mailto:info@durgasakha.com"
-          />
-          <ContactCard
-            icon={<BsTelephone size={50}/>} 
-            title="Phone Number" 
-            subtitle="+91 1234567890" 
-            callToActionText="Call us now"
-            callToActionLink="tel:+911234567890"
-          />
-          <ContactCard 
-            icon={<IoLocationOutline size={50}/>} 
-            title="Location" 
-            subtitle="Durgasakha Charitable Trust Cosmos County CASA, B Wing, flat no 1C, Ovla GB Road, Thane (west) – 400615"
-            callToActionText="Find us here"
-            callToActionLink="mailto:info@durgasakha.com"
-          />
-        </div>
+  return (
+    <div className="flex flex-col items-center border-red-600 pb-24">
+      <Map />
+      <EmailForm />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <ContactCard
+          icon={<MdOutlineEmail size={50} />}
+          title="Email Address"
+          subtitle="info@durgasakha.com"
+          callToActionText="Send Email"
+          callToActionLink="mailto:info@durgasakha.com"
+        />
+        <ContactCard
+          icon={<BsTelephone size={50} />}
+          title="Phone Number"
+          subtitle="+91 1234567890"
+          callToActionText="Call us now"
+          callToActionLink="tel:+911234567890"
+        />
+        <ContactCard
+          icon={<IoLocationOutline size={50} />}
+          title="Location"
+          subtitle="Durgasakha Charitable Trust Cosmos County CASA, B Wing, flat no 1C, Ovla GB Road, Thane (west) – 400615"
+          callToActionText="Find us here"
+          callToActionLink="mailto:info@durgasakha.com"
+        />
+      </div>
     </div>
   );
 }
-  
