74846a5dccd9cd823680d026ec32af1d35f5925f
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-12-10 02:36:15 +0530

    gallery api integration complete

--- src/app/components/gallery/galleryAllCard.js
+++ src/app/components/gallery/galleryAllCard.js
@@ -1,30 +1,37 @@
-import Image from 'next/image';
-import Link from 'next/link';
-const GalleryAllCard = ({title, imageUrl}) => {
-    return (
-        <div>
-            <div className="relative max-full overflow-hidden rounded-lg bg-white shadow-lg">
-                {/* Background Image Section */}
-                <div className="relative h-[300px]">
-                    <div className='h-full w-full'>
-                        <Image
-                            src={imageUrl}
-                            alt={'Social Work'}
-                            className="object-cover h-full w-full"
-                            width={600}
-                            height={400}
-                        />
-                    </div>
+import Image from "next/image";
+import Link from "next/link";
 
-                </div>
-
-                {/* Content Section */}
-                <div className="p-6">
-                    <div className="text-sm font-bold">{title}</div>
-                </div>
+export default function GalleryAllCard({ data }) {
+  return (
+    <div>
+      <Link href={`/gallery/${data.type}s/${data.id}`}>
+        <div className="relative max-full overflow-hidden rounded-lg bg-white shadow-lg">
+          {/* Background Image Section */}
+          <div className="relative h-[300px]">
+            <div className="h-full w-full">
+              <Image
+                src={data.gallery[0].image}
+                alt={"Social Work"}
+                className="object-cover h-full w-full"
+                width={600}
+                height={400}
+              />
             </div>
-        </div>
-    )
-}
+          </div>
 
-export default GalleryAllCard;
+          {/* Content Section */}
+          <div className="flex flex-row justify-between p-6">
+            <div className="text-md font-bold">{data.name}</div>
+            <div className="text-sm text-gray-600">
+              {new Date(data.start_date).toLocaleDateString("en-GB", {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+              })}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,79 +1,19 @@
-import GalleryAllCard from '../components/gallery/galleryAllCard';
+import GalleryAllCard from "../components/gallery/galleryAllCard";
+import { API_URL } from "../config/api";
 
-const GalleryData = [
-    {
-        "title": "Everest Mountain",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Kilimanjaro Expedition",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Amazon Jungle Trek",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Sahara Desert Adventure",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Rocky Mountain Hike",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Grand Canyon Exploration",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Great Wall of China Tour",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Patagonia Wilderness Journey",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Swiss Alps Ski Trip",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "New Zealand Hiking Tour",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Machu Picchu Exploration",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Andes Mountain Expedition",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Iceland Volcano Tour",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "African Safari Adventure",
-        "imageUrl": "/event.png"
-    },
-    {
-        "title": "Himalayas Base Camp Trek",
-        "imageUrl": "/event.png"
-    }
-];
+export default async function Gallery() {
+  const data = await fetch(`${API_URL}/api/core/gallery`).then((res) =>
+    res.json()
+  );
 
-
-export default function Gallery() {
   return (
-    <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8'>
-      {GalleryData.map((item, index) => (
-                <GalleryAllCard 
-                    key={index} 
-                    title={item.title} 
-                    imageUrl={item.imageUrl} 
-                />
-            ))}
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
+      {data.results.map((item, index) => (
+        <GalleryAllCard
+          key={index}
+          data={item}
+        />
+      ))}
     </div>
   );
 }
