caccb9656fd1129f6425dd8cd7d63d4982db20a9
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-12-10 15:52:00 +0530

    integrated carousel api

--- src/app/components/carousel/Carousel.js
+++ src/app/components/carousel/Carousel.js
@@ -1,46 +1,45 @@
 import { Button, Carousel } from "flowbite-react";
-import Image from 'next/image';
-import styles from "./Carousel.module.css"
+import Image from "next/image";
+import styles from "./Carousel.module.css";
+import { API_URL } from "@/app/config/api";
+import Link from "next/link";
 
-export function CustomCarousel() {
-
-  const carouselData = [
-    {
-      imageSrc : "/hero.png",
-      title : "Discover Your Next Adventure with Durgasakha",
-      subtitle : "We're a travel company driven by a mission to explore responsibly and make a difference. Join us in our journey to travel with heart and leave a positive impact.",
-    },
-    {
-      imageSrc: "/nature.jpg",
-      title: "Hello World",
-      subtitle: "hello world",
-    },
-    {
-      imageSrc: "https://flowbite.com/docs/images/carousel/carousel-2.svg",
-      title: "Hello World",
-      subtitle: "hello world",
-    },
-    {
-      imageSrc: "https://flowbite.com/docs/images/carousel/carousel-2.svg",
-      title: "Hello World",
-      subtitle: "hello world",
-    },
-  ]
+export async function CustomCarousel() {
+  const carouselData = await fetch(`${API_URL}/api/core/carousel`).then((res) =>
+    res.json()
+  );
 
   return (
     <div className="h-96 md:h-[600px]">
-      <Carousel slide={false} className={styles['carousel-no-rounded']}>
-        {carouselData.map((item, index)=>(
+      <Carousel slide={false} className={styles["carousel-no-rounded"]}>
+        {carouselData.map((item, index) => (
           <div key={index} className="relative h-full">
-            <Image src={item.imageSrc} alt="..." fill={true} style={{
-              objectFit: "cover",
-            }} />
+            <Image
+              src={item.image}
+              alt="..."
+              fill={true}
+              style={{
+                objectFit: "cover",
+              }}
+            />
             <div className="absolute inset-0 flex flex-col items-left justify-center pl-6 lg:pl-20">
-              <h2 className="max-w-4xl text-4xl md:text-7xl font-bold text-white">{item.title}</h2>
-              <p className="mt-3 text-sm md:text-lg text-white border-l-4 border-secondary pl-4 max-w-sm md:max-w-xl">{item.subtitle}</p>
+              <h2 className="max-w-4xl text-4xl md:text-7xl font-bold text-white">
+                {item.title}
+              </h2>
+              <p className="mt-3 text-sm md:text-lg text-white border-l-4 border-secondary pl-4 max-w-sm md:max-w-xl">
+                {item.sub_title}
+              </p>
               <div className="flex flex-row space-x-4 pl-8 mt-4">
-                <Button className="rounded-full bg-secondary text-white">Book Now</Button>
-                <Button className="rounded-full bg-white text-primary hover:text-white">View Treks</Button>
+                <Link href={item.button1_link}>
+                  <Button className="rounded-full bg-secondary text-white">
+                    {item.button1_text}
+                  </Button>
+                </Link>
+                <Link href={item.button2_link}>
+                  <Button className="rounded-full bg-white text-primary hover:text-white">
+                    {item.button2_text}
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
