e9fff2ddcd010d44f89a5707e23b275af4835cf0
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-12-10 13:00:51 +0530

    integrated upcoming treks & events api in home page

---
+++ src/app/components/home/homeCardEvent.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function HomeCardTrek({ event }) {
+  return (
+    <Link href={`/events/${event.id}`}>
+      <div className="trek-card bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="relative flex justify-center items-center">
+          <Image
+            src={event.event_image}
+            className="h-[286px] w-full object-cover"
+            alt={`${event.name} cover`}
+            width={500}
+            height={286}
+          />
+        </div>
+        <div className="trek-details p-4 flex flex-col gap-1 md:p-6">
+          <p className="lg:text-[20px] md:text-base font-semibold text-gray-600">
+            {event.venue}
+          </p>
+          <h2 className="lg:text-[35px] sm:text-xl font-bold text-primary mb-2">
+            {event.name}
+          </h2>
+          <div className="flex justify-between mt-2 text-sm md:text-base text-justify">
+            <div className="flex items-center gap-1 text-md">
+              <svg
+                width="17"
+                height="17"
+                viewBox="0 0 17 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4.55893 1.03516C4.55893 0.759014 4.33507 0.535156 4.05893 0.535156C3.78279 0.535156 3.55893 0.759014 3.55893 1.03516V1.53516H2.55893C1.45436 1.53516 0.558929 2.43059 0.558929 3.53516V4.53516H16.5589V3.53516C16.5589 2.43059 15.6635 1.53516 14.5589 1.53516H13.5589V1.03516C13.5589 0.759014 13.3351 0.535156 13.0589 0.535156C12.7828 0.535156 12.5589 0.759014 12.5589 1.03516V1.53516H4.55893V1.03516Z"
+                  fill="#D8423F"
+                />
+                <path
+                  d="M16.5589 14.5352V5.53516H0.558929V14.5352C0.558929 15.6397 1.45436 16.5352 2.55893 16.5352H14.5589C15.6635 16.5352 16.5589 15.6397 16.5589 14.5352ZM9.05893 9.03516V10.5352H10.5589C10.8351 10.5352 11.0589 10.759 11.0589 11.0352C11.0589 11.3113 10.8351 11.5352 10.5589 11.5352H9.05893V13.0352C9.05893 13.3113 8.83507 13.5352 8.55893 13.5352C8.28279 13.5352 8.05893 13.3113 8.05893 13.0352V11.5352H6.55893C6.28279 11.5352 6.05893 11.3113 6.05893 11.0352C6.05893 10.759 6.28279 10.5352 6.55893 10.5352H8.05893V9.03516C8.05893 8.75901 8.28279 8.53516 8.55893 8.53516C8.83507 8.53516 9.05893 8.75901 9.05893 9.03516Z"
+                  fill="#D8423F"
+                />
+              </svg>
+              <span className="text-md text-gray-600">
+                {new Date(event.start_date).toLocaleDateString("en-GB", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { CustomCarousel } from "./components/carousel/Carousel";
-import HomeCard from "./components/home/homeCard";
+import HomeCardTrek from "./components/home/homeCardTrek";
+import HomeCardEvent from "./components/home/homeCardEvent";
 import StarRating from "./components/home/starRating";
 import TestimonialSlider from "./components/home/testimonialSlider";
 import Image from "next/image";
-import { getTrek, getTreks } from "./services/trek";
+import { API_URL } from "./config/api";
+import Link from "next/link";
 
 export default async function Home() {
   const features = [
@@ -13,13 +15,18 @@ export default async function Home() {
     "Positive Impact",
   ];
 
-  const treks = await getTreks();
-  console.log(treks);
+  const upcomingTreks = await fetch(`${API_URL}/api/trek/upcoming`).then(
+    (res) => res.json()
+  );
+
+  const upcomingEvents = await fetch(`${API_URL}/api/event/upcoming`).then(
+    (res) => res.json()
+  );
 
   return (
     <div className="home">
       <CustomCarousel />
-      
+
       <section className="welcome p-6 md:p-16 space-y-10 grid sm:grid-cols-1 md:grid-cols-2 gap-4 items-center">
         <div className="welcome-image">
           <div className="relative w-full h-[300px] md:h-[400px] lg:h-[500px] flex justify-center items-center">
@@ -82,14 +89,16 @@ export default async function Home() {
             ))}
           </div>
           <p className="mt-2 text-sm md:text-lg border-l-4 border-secondary pl-4 md:pl-8 text-justify max-w-7xl">
-            Where Adventure Meets Purpose! We&apos;re thrilled to have you here. At
-            Durgasakha, we&apos;re more than just a travel company. We&apos;re a community
-            of adventurers committed to exploring the world responsibly and
-            making a positive impact along the way.
+            Where Adventure Meets Purpose! We&apos;re thrilled to have you here.
+            At Durgasakha, we&apos;re more than just a travel company.
+            We&apos;re a community of adventurers committed to exploring the
+            world responsibly and making a positive impact along the way.
           </p>
-          <button className="self-start border border-1 bg-white text-primary font-bold px-4 py-2 rounded-3xl">
-            Learn More
-          </button>
+          <Link href={`/about-us`}>
+            <button className="self-start border border-1 bg-white text-primary font-bold px-4 py-2 rounded-3xl">
+              Learn More
+            </button>
+          </Link>
         </div>
       </section>
 
@@ -99,18 +108,30 @@ export default async function Home() {
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-
-          <HomeCard title="Tringlewadi Trek" subtitle="Mountain Trek" organisers="Rishabh Sinha" date="12 March 2024" image="/nature.jpg" />
-          <HomeCard title="Tringlewadi Trek" subtitle="Mountain Trek" organisers="Rishabh Sinha" date="12 March 2024" image="/nature.jpg" />
-          <HomeCard title="Tringlewadi Trek" subtitle="Mountain Trek" organisers="Rishabh Sinha" date="12 March 2024" image="/nature.jpg" />
-
+          {upcomingTreks.results.slice(0, 3).map((trek) => (
+            <HomeCardTrek key={trek.id} trek={trek} />
+          ))}
         </div>
         <div className="text-white flex justify-end">
-          <button className="flex items-center gap-2 border border-white px-4 py-2 rounded-full">View All
-            <svg width="16" height="16" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" clipRule="evenodd" d="M20.7204 10.7779C21.0932 10.2388 21.0932 9.36466 20.7204 8.82551L14.9931 0.54203C14.6204 0.00287783 14.016 0.0028778 13.6432 0.54203C13.2704 1.08118 13.2704 1.95531 13.6432 2.49446L17.741 8.42115L0.954546 8.42115C0.427367 8.42115 3.53701e-07 9.03925 3.27835e-07 9.80173C3.0197e-07 10.5642 0.427367 11.1823 0.954546 11.1823L17.741 11.1823L13.6432 17.109C13.2704 17.6481 13.2704 18.5223 13.6432 19.0614C14.016 19.6006 14.6204 19.6006 14.9931 19.0614L20.7204 10.7779Z" fill="white" />
-            </svg>
-          </button>
+          <Link href={`/upcoming-treks`}>
+            <button className="flex items-center gap-2 border border-white px-4 py-2 rounded-full">
+              View All
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 21 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M20.7204 10.7779C21.0932 10.2388 21.0932 9.36466 20.7204 8.82551L14.9931 0.54203C14.6204 0.00287783 14.016 0.0028778 13.6432 0.54203C13.2704 1.08118 13.2704 1.95531 13.6432 2.49446L17.741 8.42115L0.954546 8.42115C0.427367 8.42115 3.53701e-07 9.03925 3.27835e-07 9.80173C3.0197e-07 10.5642 0.427367 11.1823 0.954546 11.1823L17.741 11.1823L13.6432 17.109C13.2704 17.6481 13.2704 18.5223 13.6432 19.0614C14.016 19.6006 14.6204 19.6006 14.9931 19.0614L20.7204 10.7779Z"
+                  fill="white"
+                />
+              </svg>
+            </button>
+          </Link>
         </div>
       </section>
 
@@ -120,49 +141,66 @@ export default async function Home() {
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-
-          <HomeCard title="Vidya School" subtitle="School Kit Distribution" organisers="Rishabh Sinha" date="12 March 2024" image="/event.png" />
-          <HomeCard title="Vidya School" subtitle="School Kit Distribution" organisers="Rishabh Sinha" date="12 March 2024" image="/event.png" />
-          <HomeCard title="Vidya School" subtitle="School Kit Distribution" organisers="Rishabh Sinha" date="12 March 2024" image="/event.png" />
-
+          {upcomingEvents.results.slice(0, 3).map((event) => (
+            <HomeCardEvent key={event.id} event={event} />
+          ))}
         </div>
         <div className="flex justify-end">
-          <button className="flex items-center gap-2 border border-black px-4 py-2 rounded-full">View All
-            <svg width="16" height="16" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" clipRule="evenodd" d="M20.7204 11.6143C21.0932 11.0751 21.0932 10.201 20.7204 9.66185L14.9931 1.37836C14.6204 0.839212 14.016 0.839212 13.6432 1.37836C13.2704 1.91751 13.2704 2.79165 13.6432 3.3308L17.741 9.25748L0.954546 9.25748C0.427367 9.25748 3.53701e-07 9.87559 3.27835e-07 10.6381C3.0197e-07 11.4005 0.427367 12.0186 0.954546 12.0186L17.741 12.0186L13.6432 17.9453C13.2704 18.4845 13.2704 19.3586 13.6432 19.8978C14.016 20.4369 14.6204 20.4369 14.9931 19.8978L20.7204 11.6143Z" fill="black" />
-            </svg>
-
-          </button>
+          <Link href={`/upcoming-events`}>
+            <button className="flex items-center gap-2 border border-black px-4 py-2 rounded-full">
+              View All
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M20.7204 11.6143C21.0932 11.0751 21.0932 10.201 20.7204 9.66185L14.9931 1.37836C14.6204 0.839212 14.016 0.839212 13.6432 1.37836C13.2704 1.91751 13.2704 2.79165 13.6432 3.3308L17.741 9.25748L0.954546 9.25748C0.427367 9.25748 3.53701e-07 9.87559 3.27835e-07 10.6381C3.0197e-07 11.4005 0.427367 12.0186 0.954546 12.0186L17.741 12.0186L13.6432 17.9453C13.2704 18.4845 13.2704 19.3586 13.6432 19.8978C14.016 20.4369 14.6204 20.4369 14.9931 19.8978L20.7204 11.6143Z"
+                  fill="black"
+                />
+              </svg>
+            </button>
+          </Link>
         </div>
       </section>
 
       <section className="volunteer p-6 md:p-16 space-y-10 grid sm:grid-cols-1 lg:grid-cols-2 lg:gap-36 bg-[#EBEBEB]">
         <div className="flex flex-col justify-center">
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-6 md:mb-10">
-          Volunteer with Durgasakha
+            Volunteer with Durgasakha
           </h1>
           <p className="mt-2 text-sm md:text-lg border-l-4 text-secondary font-bold border-secondary pl-4 md:pl-8 text-justify max-w-7xl">
             Experience the Joy of Giving Back While Exploring the World
           </p>
           <p className="mt-4 text-sm md:text-lg font-semibold text-justify max-w-7xl">
-            At Durgasakha, we believe that travel is not just about sightseeing; it&apos;s also about giving back to the places we visit. Our volunteer program offers you the opportunity to immerse yourself in diverse cultures while making a meaningful impact on local communities and the environment.
+            At Durgasakha, we believe that travel is not just about sightseeing;
+            it&apos;s also about giving back to the places we visit. Our
+            volunteer program offers you the opportunity to immerse yourself in
+            diverse cultures while making a meaningful impact on local
+            communities and the environment.
           </p>
         </div>
 
         <div className="flex justify-center">
-        <Image
-          src="/nature.jpg"
-          alt="Top Left"
-          className="object-cover w-full h-full self-center shadow-lg"
-          width={500}
-          height={500}
-        />
+          <Image
+            src="/nature.jpg"
+            alt="Top Left"
+            className="object-cover w-full h-full self-center shadow-lg"
+            width={500}
+            height={500}
+          />
         </div>
       </section>
 
       <section className="testimonial p-6 md:p-16 space-y-10">
         <div className="text-center mb-16">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-6 md:mb-10">What our Trekkers say about us</h1>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-primary mb-6 md:mb-10">
+            What our Trekkers say about us
+          </h1>
         </div>
         <TestimonialSlider />
       </section>
