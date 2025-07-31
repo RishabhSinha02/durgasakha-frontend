f8575090833c84f3064ecf55176db59dda31c784
Om Avhad <omavhad22@gmail.com>
2025-07-31 17:41:13 +0530

    fix

--- src/app/donate/layout.js
+++ src/app/donate/layout.js
@@ -1,16 +1,37 @@
-import { Header } from '../components/header/Header';
+import { Header } from "../components/header/Header";
+
+export const metadata = {
+  metadataBase: new URL("https://www.durgasakha.org"),
+  title: "Donation | Shree Durgasakha Charitable Trust",
+  description:
+    "Durgasakha is an NGO committed to preserving the forts of the Sahyadri region through cleanliness drives and awareness campaigns. With over 150 treks and 1,500+ participants, it promotes a strong bond with nature, history, and responsible travel. The NGO also supports rural education in Shahapur, having distributed school kits to over 2,500 students in the past 15 years. Its educational adoption programs further help students pursue higher studies, combining environmental conservation          with social development.",
+  keywords: [
+    "Donation",
+    "Durgasakha",
+    "Shree Durgasakha Charitable Trust",
+    "NGO",
+    "donate",
+    "support",
+    "charity",
+    "social development",
+    "rural education",
+    "environmental conservation",
+  ],
+  openGraph: {
+    title: "Donation | Shree Durgasakha Charitable Trust",
+    description:
+      "Durgasakha is an NGO committed to preserving the forts of the Sahyadri region through cleanliness drives and awareness campaigns. With over 150 treks and 1,500+ participants, it promotes a strong bond with nature, history, and responsible travel. The NGO also supports rural education in Shahapur, having distributed school kits to over 2,500 students in the past 15 years. Its educational adoption programs further help students pursue higher studies, combining environmental conservation with social development.",
+    images: "/donation-banner.jpeg",
+  },
+};
 
 const Layout = ({ children }) => {
-    return (
-        <div>
-            <Header 
-                background="/header.png" 
-                title="Donation"/>
-            <main>
-                {children}
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <Header background="/header.png" title="Donation" />
+      <main>{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
--- src/app/layout.js
+++ src/app/layout.js
@@ -22,10 +22,22 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  metadataBase: new URL("https://www.durgasakha.org"), 
+  metadataBase: new URL("https://www.durgasakha.org"),
   title: "Shree Durgasakha Charitable Trust",
   description:
     "Durgasakha is an NGO committed to preserving the forts of the Sahyadri region through cleanliness drives and awareness campaigns. With over 150 treks and 1,500+ participants, it promotes a strong bond with nature, history, and responsible travel. The NGO also supports rural education in Shahapur, having distributed school kits to over 2,500 students in the past 15 years. Its educational adoption programs further help students pursue higher studies, combining environmental conservation with social development.",
+  keywords: [
+    "Durgasakha",
+    "Shree Durgasakha Charitable Trust",
+    "NGO",
+    "Sahyadri forts",
+    "cleanliness drives",
+    "awareness campaigns",
+    "trekking",
+    "nature conservation",
+    "rural education",
+    "Maharashtra",
+  ],
   openGraph: {
     title: "Shree Durgasakha Charitable Trust",
     description:
